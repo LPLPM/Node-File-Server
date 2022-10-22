1194a8bb12a4ab172a90f5c6a3b2120ef4e8e59c
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2022-10-22 22:39:18 +0200

    fix erreur varible

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const serveIndex = require('serve-index')
 
-let sharing_dir = "/app/sharing";
+let sharing_path = "/app/sharing";
 let template_path = "/app/directory.html";
-// let template_path = "/Users/leopaul/Desktop/node_file_server/directory.html";
-// let sharing_path  = "/Users/leopaul/Desktop/public";
+
 let filter = function (filename, index, files, dir){
   return !filename.toLowerCase().includes("@eadir");
 }
